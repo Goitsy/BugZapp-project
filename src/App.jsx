d7c16b7f2f6a2b64eb6aa94e6e575d83c505b201
Goitsy <goitseone.montsho@dci-student.org>
2024-10-01 15:04:27 +0200

    empty comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>BugZapp</h1>
       <p>App is bulding...</p>
     </>
